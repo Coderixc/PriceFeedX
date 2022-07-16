e22a55ac536c3b145f766bc98d1ecd70eefa4fa6
kamal chanchal <kchanchal78@gmail.com>
2022-07-16 12:20:44 +0530

    Gui Update

--- PriceFeedX/Form1.Designer.cs
+++ PriceFeedX/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2_BHavCopyPanel = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
@@ -39,46 +38,45 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1_Nse_TOP_XX_LIST = new System.Windows.Forms.TextBox();
             this.Label_BHAVCOPYPATH = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6_Last5PrevPrice = new System.Windows.Forms.Label();
             this.panel2_BHavCopyPanel.SuspendLayout();
             this.panel3_LOAD_BHAVCOPY_TOPXX.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.panel1.Location = new System.Drawing.Point(13, 28);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1049, 126);
-            this.panel1.TabIndex = 0;
             // 
             // panel2_BHavCopyPanel
             // 
             this.panel2_BHavCopyPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2_BHavCopyPanel.Controls.Add(this.label6_Last5PrevPrice);
             this.panel2_BHavCopyPanel.Controls.Add(this.label3);
             this.panel2_BHavCopyPanel.Controls.Add(this.button2);
             this.panel2_BHavCopyPanel.Controls.Add(this.textBox1_BHAVCOPY_EQ);
             this.panel2_BHavCopyPanel.Controls.Add(this.label2);
-            this.panel2_BHavCopyPanel.Location = new System.Drawing.Point(15, 476);
-            this.panel2_BHavCopyPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2_BHavCopyPanel.Location = new System.Drawing.Point(8, 56);
+            this.panel2_BHavCopyPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2_BHavCopyPanel.Name = "panel2_BHavCopyPanel";
-            this.panel2_BHavCopyPanel.Size = new System.Drawing.Size(1049, 89);
+            this.panel2_BHavCopyPanel.Size = new System.Drawing.Size(787, 146);
             this.panel2_BHavCopyPanel.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(677, 59);
+            this.label3.Location = new System.Drawing.Point(473, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(350, 16);
+            this.label3.Size = new System.Drawing.Size(282, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "* Press Enter to load text files from Location(Equity csv file)";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(947, 34);
+            this.button2.Location = new System.Drawing.Point(710, 28);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(80, 22);
+            this.button2.Size = new System.Drawing.Size(61, 20);
             this.button2.TabIndex = 2;
             this.button2.Text = "Enter";
             this.button2.UseVisualStyleBackColor = true;
@@ -86,17 +84,19 @@
             // 
             // textBox1_BHAVCOPY_EQ
             // 
-            this.textBox1_BHAVCOPY_EQ.Location = new System.Drawing.Point(201, 34);
+            this.textBox1_BHAVCOPY_EQ.Location = new System.Drawing.Point(151, 28);
+            this.textBox1_BHAVCOPY_EQ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1_BHAVCOPY_EQ.Name = "textBox1_BHAVCOPY_EQ";
-            this.textBox1_BHAVCOPY_EQ.Size = new System.Drawing.Size(724, 22);
+            this.textBox1_BHAVCOPY_EQ.Size = new System.Drawing.Size(544, 20);
             this.textBox1_BHAVCOPY_EQ.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Location = new System.Drawing.Point(2, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(169, 16);
+            this.label2.Size = new System.Drawing.Size(137, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "BHAV COPY NSE EQ(.csv)";
             // 
@@ -107,28 +107,26 @@
             this.panel3_LOAD_BHAVCOPY_TOPXX.Controls.Add(this.button1);
             this.panel3_LOAD_BHAVCOPY_TOPXX.Controls.Add(this.textBox1_Nse_TOP_XX_LIST);
             this.panel3_LOAD_BHAVCOPY_TOPXX.Controls.Add(this.Label_BHAVCOPYPATH);
-            this.panel3_LOAD_BHAVCOPY_TOPXX.Location = new System.Drawing.Point(17, 574);
-            this.panel3_LOAD_BHAVCOPY_TOPXX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3_LOAD_BHAVCOPY_TOPXX.Location = new System.Drawing.Point(8, 206);
+            this.panel3_LOAD_BHAVCOPY_TOPXX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3_LOAD_BHAVCOPY_TOPXX.Name = "panel3_LOAD_BHAVCOPY_TOPXX";
-            this.panel3_LOAD_BHAVCOPY_TOPXX.Size = new System.Drawing.Size(1049, 113);
+            this.panel3_LOAD_BHAVCOPY_TOPXX.Size = new System.Drawing.Size(787, 97);
             this.panel3_LOAD_BHAVCOPY_TOPXX.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(639, 50);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(471, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(386, 16);
+            this.label1.Size = new System.Drawing.Size(313, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "* Press Enter to load text file from location(ind_nifty200list_xx.csv)";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(945, 21);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(709, 18);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 25);
+            this.button1.Size = new System.Drawing.Size(60, 19);
             this.button1.TabIndex = 2;
             this.button1.TabStop = false;
             this.button1.Text = "Enter";
@@ -137,44 +135,80 @@
             // 
             // textBox1_Nse_TOP_XX_LIST
             // 
-            this.textBox1_Nse_TOP_XX_LIST.Location = new System.Drawing.Point(197, 22);
-            this.textBox1_Nse_TOP_XX_LIST.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1_Nse_TOP_XX_LIST.Location = new System.Drawing.Point(148, 18);
             this.textBox1_Nse_TOP_XX_LIST.Name = "textBox1_Nse_TOP_XX_LIST";
-            this.textBox1_Nse_TOP_XX_LIST.Size = new System.Drawing.Size(726, 22);
+            this.textBox1_Nse_TOP_XX_LIST.Size = new System.Drawing.Size(546, 20);
             this.textBox1_Nse_TOP_XX_LIST.TabIndex = 1;
             // 
             // Label_BHAVCOPYPATH
             // 
             this.Label_BHAVCOPYPATH.AutoSize = true;
-            this.Label_BHAVCOPYPATH.Location = new System.Drawing.Point(4, 26);
-            this.Label_BHAVCOPYPATH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label_BHAVCOPYPATH.Location = new System.Drawing.Point(3, 21);
             this.Label_BHAVCOPYPATH.Name = "Label_BHAVCOPYPATH";
-            this.Label_BHAVCOPYPATH.Size = new System.Drawing.Size(121, 16);
+            this.Label_BHAVCOPYPATH.Size = new System.Drawing.Size(101, 13);
             this.Label_BHAVCOPYPATH.TabIndex = 0;
             this.Label_BHAVCOPYPATH.Text = "NSE TOP 200 LIST";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(9, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(785, 48);
+            this.panel1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(300, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "NSE BHAV COPY TOOL 1.0.1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label5";
+            // 
+            // label6_Last5PrevPrice
+            // 
+            this.label6_Last5PrevPrice.AutoSize = true;
+            this.label6_Last5PrevPrice.Location = new System.Drawing.Point(145, 108);
+            this.label6_Last5PrevPrice.Name = "label6_Last5PrevPrice";
+            this.label6_Last5PrevPrice.Size = new System.Drawing.Size(86, 13);
+            this.label6_Last5PrevPrice.TabIndex = 4;
+            this.label6_Last5PrevPrice.Text = "Last 5 Prev Day:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1219, 825);
+            this.ClientSize = new System.Drawing.Size(796, 305);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3_LOAD_BHAVCOPY_TOPXX);
             this.Controls.Add(this.panel2_BHavCopyPanel);
-            this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NSE BHAV COPY 1.0.1";
             this.panel2_BHavCopyPanel.ResumeLayout(false);
             this.panel2_BHavCopyPanel.PerformLayout();
             this.panel3_LOAD_BHAVCOPY_TOPXX.ResumeLayout(false);
             this.panel3_LOAD_BHAVCOPY_TOPXX.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2_BHavCopyPanel;
         private System.Windows.Forms.Panel panel3_LOAD_BHAVCOPY_TOPXX;
         private System.Windows.Forms.Label label1;
@@ -185,6 +219,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1_BHAVCOPY_EQ;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6_Last5PrevPrice;
+        private System.Windows.Forms.Label label5;
     }
 }
 
