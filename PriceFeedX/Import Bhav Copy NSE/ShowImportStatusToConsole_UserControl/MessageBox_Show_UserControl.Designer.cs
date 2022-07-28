09c9302afd6d37872df5c0e2f467ef1480566bf7
kamal chanchal <kchanchal78@gmail.com>
2022-07-28 08:16:57 +0530

    Adding User COntrol Panel to Project  Progress bar of NSE Bhav Copy Downloaded

---
+++ PriceFeedX/Import Bhav Copy NSE/ShowImportStatusToConsole_UserControl/MessageBox_Show_UserControl.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PriceFeedX.Import_Bhav_Copy_NSE.ShowImportStatusToConsole_UserControl
+{
+    partial class MessageBox_Show_UserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.eventLog2_User_Control = new System.Diagnostics.EventLog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog2_User_Control)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(3, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(879, 296);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Importing Progress ";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(863, 46);
+            this.panel2.TabIndex = 1;
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // eventLog2_User_Control
+            // 
+            this.eventLog2_User_Control.SynchronizingObject = this;
+            // 
+            // MessageBox_Show_UserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "MessageBox_Show_UserControl";
+            this.Size = new System.Drawing.Size(890, 316);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog2_User_Control)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Diagnostics.EventLog eventLog2_User_Control;
+    }
+}
---
+++ PriceFeedX/Import Bhav Copy NSE/ShowImportStatusToConsole_UserControl/MessageBox_Show_UserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PriceFeedX.Import_Bhav_Copy_NSE.ShowImportStatusToConsole_UserControl
+{
+    public partial class MessageBox_Show_UserControl : UserControl
+    {
+        public MessageBox_Show_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
