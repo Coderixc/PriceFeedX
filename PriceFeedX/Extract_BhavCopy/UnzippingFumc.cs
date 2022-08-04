8d6d314bb65842debe3f4107c544540224cbca15
kamal chanchal <kchanchal78@gmail.com>
2022-08-04 12:18:22 +0530

    add Constructor To load main directory  where a Bhav copy are laoded.
    Lading all Directory , whcih will be directed  to user, from there it will be selected which needed to be worked on.

--- PriceFeedX/Extract_BhavCopy/UnzippingFumc.cs
+++ PriceFeedX/Extract_BhavCopy/UnzippingFumc.cs
@@ -9,17 +9,37 @@ namespace PriceFeedX.Extract_BhavCopy
 {
     //Object- YES
     //Inheritance  - NO
-    sealed class UnzippingFumc
+    sealed class UnzippingFunc
     {
 
          private string m_unZippingPath = String .Empty;    
 
 
-        public UnzippingFumc() //HOW TO GET WHICH FOLDER THE LAST FOLDER WHICH WAS USED TO IMPORTING BHAV COPY
+        public UnzippingFunc() //HOW TO GET WHICH FOLDER THE LAST FOLDER WHICH WAS USED TO IMPORTING BHAV COPY
         {
-            this.m_unZippingPath = DumpFolder.
+            this.m_unZippingPath = DumpFolder.Dump_Path;  // using main path not exact path of bhav copy path
 
         }
+
+        private  string Load_All_Directory_which_contain_Bhavcopy()
+        {
+            string result = string.Empty;   
+            try
+            {
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                return string.Empty;
+
+            }
+
+        }
+
+
+
 
   
 
